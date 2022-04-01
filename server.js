dfab567cedfbbae8eeaa71c44f656ae706148d8d
Nuel <ibk.com@gmail.com>
2022-04-01 16:09:01 +0100

    fixed exercise create typo error

--- server.js
+++ server.js
@@ -125,7 +125,7 @@ app.post('/api/users/:_id/exercises', (req, res) => {
   console.log(params);
 
   // Find the user..
-  User.findById(reqparams._id, (err, user) => {
+  User.findById(req.params._id, (err, user) => {
     if (err) {
       res.send({ error: err }); res.end();
     }
